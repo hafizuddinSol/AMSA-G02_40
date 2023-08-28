0f321ab0aa5a4a6a3742720c9a299cbeed6e94be
NajmiHakim19 <118854094+NajmiHakim19@users.noreply.github.com>
2023-08-28 10:29:12 +0800

    Update welcome splash screen text

--- smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
+++ smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
@@ -45,7 +45,7 @@ class WelcomeScreen extends StatelessWidget {
                     padding: EdgeInsets.only(
                         left: 16, top: 32, right: 16, bottom: 8),
                     child: Text(
-                      'Say Hello To Your New App!',
+                      'Welcome to your AquaManager app!',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                           color: Color(colorPrimary),
@@ -56,7 +56,7 @@ class WelcomeScreen extends StatelessWidget {
                   const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
                     child: Text(
-                      'You\'ve just saved a week of development and headaches.',
+                      'Your all-in-one aquarium management app.',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center,
                     ),
