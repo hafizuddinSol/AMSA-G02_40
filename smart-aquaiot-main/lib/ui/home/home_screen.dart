7a0cd30b50f2801473c35690506cd477df15357f
afiqerinoor7 <notafiqerinoor@gmail.com>
2023-09-03 00:44:05 +0800

    Homepage UI

--- smart-aquaiot-main/lib/ui/home/home_screen.dart
+++ smart-aquaiot-main/lib/ui/home/home_screen.dart
@@ -14,20 +14,14 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key, required this.user}) : super(key: key);
 
   @override
-  State createState() => _HomeState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeState extends State<HomeScreen> {
-  late User user;
-
-  @override
-  void initState() {
-    super.initState();
-    user = widget.user;
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final user = widget.user;
+
     return BlocListener<AuthenticationBloc, AuthenticationState>(
       listener: (context, state) {
         if (state.authState == AuthState.unauthenticated) {
@@ -35,91 +29,130 @@ class _HomeState extends State<HomeScreen> {
         }
       },
       child: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color(colorPrimary),
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  'Logout',
-                  style: TextStyle(
-                      color: isDarkMode(context)
-                          ? Colors.grey.shade50
-                          : Colors.grey.shade900),
-                ),
-                leading: Transform.rotate(
-                  angle: pi / 1,
-                  child: Icon(
-                    Icons.exit_to_app,
-                    color: isDarkMode(context)
-                        ? Colors.grey.shade50
-                        : Colors.grey.shade900,
-                  ),
-                ),
-                onTap: () {
-                  context.read<AuthenticationBloc>().add(LogoutEvent());
-                },
-              ),
-            ],
-          ),
-        ),
         appBar: AppBar(
           title: Text(
             'Home',
             style: TextStyle(
-                color: isDarkMode(context)
-                    ? Colors.grey.shade50
-                    : Colors.grey.shade900),
-          ),
-          iconTheme: IconThemeData(
               color: isDarkMode(context)
                   ? Colors.grey.shade50
-                  : Colors.grey.shade900),
+                  : Colors.grey.shade900,
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: isDarkMode(context)
+                ? Colors.grey.shade50
+                : Colors.grey.shade900,
+          ),
           backgroundColor:
               isDarkMode(context) ? Colors.grey.shade900 : Colors.grey.shade50,
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              user.profilePictureURL == ''
-                  ? CircleAvatar(
-                      radius: 35,
-                      backgroundColor: Colors.grey.shade400,
-                      child: ClipOval(
-                        child: SizedBox(
-                          width: 70,
-                          height: 70,
-                          child: Image.asset(
-                            'assets/images/placeholder.jpg',
-                            fit: BoxFit.cover,
-                          ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 160, // Set the height for the 'Edit Profile' button
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Implement Edit Profile function
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
                         ),
                       ),
-                    )
-                  : displayCircleImage(user.profilePictureURL, 80, false),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(user.fullName()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          user.profilePictureURL == ''
+                              ? CircleAvatar(
+                                  radius: 35,
+                                  backgroundColor: Colors.grey.shade400,
+                                  child: ClipOval(
+                                    child: SizedBox(
+                                      width: 70,
+                                      height: 70,
+                                      child: Image.asset(
+                                        'assets/images/placeholder.jpg',
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : displayCircleImage(
+                                  user.profilePictureURL, 70, true),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Edit Profile',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(user.email),
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2, // 2 columns for symmetry
+                childAspectRatio: 1.0, // Square aspect ratio
+                padding: EdgeInsets.all(16.0),
+                mainAxisSpacing: 16.0,
+                crossAxisSpacing: 16.0,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Temperature function
+                    },
+                    child: Text('Temperature'),
+                    style: ElevatedButton.styleFrom(primary: Colors.red),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Water level function
+                    },
+                    child: Text('Water level'),
+                    style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Feed now function
+                    },
+                    child: Text('Feed now'),
+                    style: ElevatedButton.styleFrom(primary: Colors.orange),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Display feeding time function
+                    },
+                    child: Text('Display feeding time'),
+                    style: ElevatedButton.styleFrom(primary: Colors.brown),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement pH level function
+                    },
+                    child: Text('pH level'),
+                    style: ElevatedButton.styleFrom(primary: Colors.green),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Logout function
+                      context.read<AuthenticationBloc>().add(LogoutEvent());
+                    },
+                    style: ElevatedButton.styleFrom(primary: Colors.purple),
+                    child: Text('Logout', style: TextStyle(fontSize: 16.0)),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
