c6fdde7a7f67cfd4e9e1550f3983c1f5e0ea81da
afiqerinoor7 <notafiqerinoor@gmail.com>
2023-09-27 22:51:29 +0800

    Minor changes in welcome_screen.dart

--- smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
+++ smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_login_screen/constants.dart';
 import 'package:flutter_login_screen/services/helper.dart';
 import 'package:flutter_login_screen/ui/auth/login/login_screen.dart';
 import 'package:flutter_login_screen/ui/auth/signUp/sign_up_screen.dart';
