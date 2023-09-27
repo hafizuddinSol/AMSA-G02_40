5e087bd0497644f251f3dcf15bfcef254484032b
afiqerinoor7 <notafiqerinoor@gmail.com>
2023-09-27 20:55:21 +0800

    Add background pic for welcome page

---
+++ smart-aquaiot-main/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,105 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<cloud_firestore/FLTFirebaseFirestorePlugin.h>)
+#import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
+#else
+@import cloud_firestore;
+#endif
+
+#if __has_include(<file_picker/FilePickerPlugin.h>)
+#import <file_picker/FilePickerPlugin.h>
+#else
+@import file_picker;
+#endif
+
+#if __has_include(<firebase_auth/FLTFirebaseAuthPlugin.h>)
+#import <firebase_auth/FLTFirebaseAuthPlugin.h>
+#else
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_storage/FLTFirebaseStoragePlugin.h>)
+#import <firebase_storage/FLTFirebaseStoragePlugin.h>
+#else
+@import firebase_storage;
+#endif
+
+#if __has_include(<flutter_facebook_auth/FlutterFacebookAuthPlugin.h>)
+#import <flutter_facebook_auth/FlutterFacebookAuthPlugin.h>
+#else
+@import flutter_facebook_auth;
+#endif
+
+#if __has_include(<flutter_secure_storage/FlutterSecureStoragePlugin.h>)
+#import <flutter_secure_storage/FlutterSecureStoragePlugin.h>
+#else
+@import flutter_secure_storage;
+#endif
+
+#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
+#import <image_picker_ios/FLTImagePickerPlugin.h>
+#else
+@import image_picker_ios;
+#endif
+
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+#if __has_include(<sqflite/SqflitePlugin.h>)
+#import <sqflite/SqflitePlugin.h>
+#else
+@import sqflite;
+#endif
+
+#if __has_include(<the_apple_sign_in/TheAppleSignInPlugin.h>)
+#import <the_apple_sign_in/TheAppleSignInPlugin.h>
+#else
+@import the_apple_sign_in;
+#endif
+
+#if __has_include(<url_launcher_ios/FLTURLLauncherPlugin.h>)
+#import <url_launcher_ios/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseFirestorePlugin"]];
+  [FilePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FilePickerPlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseStoragePlugin"]];
+  [FlutterFacebookAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterFacebookAuthPlugin"]];
+  [FlutterSecureStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSecureStoragePlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [TheAppleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"TheAppleSignInPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
--- smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
+++ smart-aquaiot-main/lib/ui/auth/welcome/welcome_screen.dart
@@ -12,112 +12,154 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const TextStyle whiteTextStyle = TextStyle(
+      fontSize: 18,
+      color: Colors.white, // Set the text color to white
+    );
+
     return BlocProvider<WelcomeBloc>(
       create: (context) => WelcomeBloc(),
       child: Builder(
         builder: (context) {
           return Scaffold(
-            body: BlocListener<WelcomeBloc, WelcomeInitial>(
-              listener: (context, state) {
-                switch (state.pressTarget) {
-                  case WelcomePressTarget.login:
-                    push(context, const LoginScreen());
-                    break;
-                  case WelcomePressTarget.signup:
-                    push(context, const SignUpScreen());
-                    break;
-                  case WelcomePressTarget.adminLogin:
-                    push(context, const AdminLoginScreen());
-                    break;
-                  default:
-                    break;
-                }
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Center(
-                    child: Image.asset(
-                      'assets/images/aquarium.png',
-                      width: 150.0,
-                      height: 150.0,
+            body: Stack(
+              children: <Widget>[
+                // Background Image
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/background.jpg'),
                       fit: BoxFit.cover,
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                        left: 16, top: 32, right: 16, bottom: 8),
-                    child: Text(
-                      'Welcome to your AquaManager app!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Color(colorPrimary),
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold),
+                ),
+                // Gradient Overlay
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.black.withOpacity(0.6),
+                        Colors.transparent,
+                        Colors.black.withOpacity(0.6),
+                      ],
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                    child: Text(
-                      'Your all-in-one aquarium management app.',
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(right: 40.0, left: 40.0, top: 40),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: Size.fromWidth(
-                            MediaQuery.of(context).size.width / 1.5),
-                        backgroundColor: const Color(colorPrimary),
-                        textStyle: const TextStyle(color: Colors.white),
-                        padding: const EdgeInsets.only(top: 16, bottom: 16),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                            side: const BorderSide(color: Color(colorPrimary))),
+                ),
+                BlocListener<WelcomeBloc, WelcomeInitial>(
+                  listener: (context, state) {
+                    switch (state.pressTarget) {
+                      case WelcomePressTarget.login:
+                        push(context, const LoginScreen());
+                        break;
+                      case WelcomePressTarget.signup:
+                        push(context, const SignUpScreen());
+                        break;
+                      case WelcomePressTarget.adminLogin:
+                        push(context, const AdminLoginScreen());
+                        break;
+                      default:
+                        break;
+                    }
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: Image.asset(
+                          'assets/images/aquarium.png',
+                          width: 150.0,
+                          height: 150.0,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: const Text(
-                        'Log In',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: () {
-                        context.read<WelcomeBloc>().add(LoginPressed());
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        right: 40.0, left: 40.0, top: 20, bottom: 20),
-                    child: TextButton(
-                      onPressed: () {
-                        context.read<WelcomeBloc>().add(SignupPressed());
-                      },
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.only(top: 16, bottom: 16),
-                        fixedSize: Size.fromWidth(
-                            MediaQuery.of(context).size.width / 1.5),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          side: const BorderSide(
-                            color: Color(colorPrimary),
+                      const Padding(
+                        padding: EdgeInsets.only(
+                            left: 16, top: 32, right: 16, bottom: 8),
+                        child: Text(
+                          'Welcome to your AquaManager app!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 162, 217, 253),
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(colorPrimary)),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                        child: Text(
+                          'Your all-in-one aquarium management app.',
+                          style:
+                              whiteTextStyle, // Use the defined whiteTextStyle
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            right: 40.0, left: 40.0, top: 40),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size.fromWidth(
+                                MediaQuery.of(context).size.width / 1.5),
+                            backgroundColor: Color.fromARGB(255, 11, 156, 149),
+                            textStyle: const TextStyle(color: Colors.white),
+                            padding: const EdgeInsets.only(top: 16, bottom: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              side: const BorderSide(
+                                color: Color.fromARGB(255, 11, 156, 149),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {
+                            context.read<WelcomeBloc>().add(LoginPressed());
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            right: 40.0, left: 40.0, top: 20, bottom: 20),
+                        child: TextButton(
+                          onPressed: () {
+                            context.read<WelcomeBloc>().add(SignupPressed());
+                          },
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.only(top: 16, bottom: 16),
+                            fixedSize: Size.fromWidth(
+                                MediaQuery.of(context).size.width / 1.5),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              side: const BorderSide(
+                                color: Color.fromARGB(255, 11, 156, 149),
+                              ),
+                            ),
+                            backgroundColor: Color.fromARGB(255, 11, 156, 149),
+                          ),
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           );
         },
