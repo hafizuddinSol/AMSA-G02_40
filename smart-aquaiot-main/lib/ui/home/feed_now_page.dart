9b746902400f20d9b3eb28129a3b0e8f0bac7a1f
afiqerinoor7 <notafiqerinoor@gmail.com>
2023-11-08 10:11:22 +0800

    Feed now page(update)

--- smart-aquaiot-main/lib/ui/home/feed_now_page.dart
+++ smart-aquaiot-main/lib/ui/home/feed_now_page.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+void feedFish(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      Future.delayed(Duration(seconds: 5), () {
+        Navigator.of(context).pop();
+      });
+      return AlertDialog(
+        title: Text('Feeding Complete'),
+        content: Text('You just fed your fish! Very Good!'),
+      );
+    },
+  );
+}
+
 class FeedNowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,8 +23,19 @@ class FeedNowPage extends StatelessWidget {
         title: Text('Feed Now'),
       ),
       body: Center(
-        child: Text('You just feed you fish! Very Good!'),
+        child: ElevatedButton(
+          onPressed: () {
+            feedFish(context);
+          },
+          child: Text('Feed Now'),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: FeedNowPage(),
+  ));
 }
